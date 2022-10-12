f0fcafb0fc8e728588ff6f36058d3ea920ed0e79
Gulzhan <200103211@stu.sdu.edu.kz>
2022-10-13 02:32:00 +0600

    2.5.1

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -20,7 +20,10 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("StringFormatInvalid")
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            return
+        }
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
