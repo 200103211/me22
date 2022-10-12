873055ca46d24e88f7f928dd00bc5c47c940a8c6
Gulzhan <200103211@stu.sdu.edu.kz>
2022-10-13 02:13:36 +0600

    2.4.1

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -12,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    fun calculateTip() {
+
     }
 }
